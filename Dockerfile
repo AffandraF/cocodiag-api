dc4f3476cca713d6dd65db5c3f3ec823dd7f7583
AffandraF <affandraf@gmail.com>
2024-06-08 15:56:24 +0700

    fix: update price

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,19 @@
 FROM python:3.11-slim
 
-# Install dependencies
-RUN apt-get update && apt-get install -y \
-    wget \
-    gnupg \
-    curl \
-    unzip \
-    fontconfig \
-    locales \
-    libnss3 \
-    libxss1 \
-    libasound2 \
-    libx11-xcb1 \
-    libxcomposite1 \
-    libxcursor1 \
-    libxdamage1 \
-    libxi6 \
-    libxtst6 \
-    libappindicator3-1 \
-    libxrandr2 \
-    libgbm1 \
-    xdg-utils \
-    --no-install-recommends
-
-RUN apt-get install -y chromium
-
-RUN CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
-    wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip && \
-    unzip /tmp/chromedriver.zip -d /tmp/ && \
-    rm /tmp/chromedriver.zip && \
-    chmod +x /tmp/chromedriver
-
-# Set display port to avoid crash
 ENV DISPLAY=:99
 
+WORKDIR /app
+
 COPY requirements.txt .
+
 RUN pip install -r requirements.txt
 
 COPY . /app
-WORKDIR /app
 
-# Expose port
+RUN apt-get install -y wget
+RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+RUN apt-get install -y ./google-chrome-stable_current_amd64.deb
+
 EXPOSE 8080
 
 CMD ["gunicorn", "-b", ":8080", "main:app"]
--- app/price.py
+++ app/price.py
@@ -1,35 +1,38 @@
 from flask import Blueprint, jsonify
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-import undetected_chromedriver as uc
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from app.auth import jwt_required
 
 price_bp = Blueprint('price_bp', __name__)
 
 def get_data():
     try:
-        options = uc.ChromeOptions()
+        options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
-        driver = uc.Chrome(
-            options=options, 
-            browser_executable_path="/usr/bin/chromium", 
-            driver_executable_path="/tmp/chromedriver"
-        )
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
         url = "https://infopangan.jakarta.go.id/publik/dashboard/23"
         driver.get(url)
 
-        date = driver.find_element(By.ID, "avg_tanggal").text
-        price = driver.find_element(By.ID, "avg_value").text
-        region = driver.find_element(By.ID, "avg_object").text        
+        date = driver.find_element(By.ID, "avg_tanggal")
+        date = date.text
+
+        price = driver.find_element(By.ID, "avg_value")
+        price = price.text
+
+        region = driver.find_element(By.ID, "avg_object")
+        region = region.text        
 
         driver.quit()
 
         response = {
             "date": date,
-            "price": price,
+            "price": price,            
             "region": region
         }
 
