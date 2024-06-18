1aa9f138f3aae64523a8970989a1fb390c06c177
AffandraF <affandraf@gmail.com>
2024-06-18 13:24:44 +0700

    fix: save image

--- app/prediction.py
+++ app/prediction.py
@@ -68,8 +68,6 @@ def predict():
        
     try:
         image = Image.open(io.BytesIO(file.read()))
-        image.verify()
-        image = Image.open(io.BytesIO(file.read()))
         processed_image = prepare_image(image, target_size=(224, 224))
         predictions = model.predict(processed_image)
         predicted_class_index = np.argmax(predictions, axis=1)[0]
--- config/save_image.py
+++ config/save_image.py
@@ -1,7 +1,5 @@
-import io
 import uuid
 import logging
-from PIL import Image
 from firebase_admin import storage
 from flask import jsonify
 
@@ -11,18 +9,15 @@ def allowed_file(filename):
 
 def save_image(image_file, user_id, path):    
     try:
-        image = Image.open(io.BytesIO(image_file.read()))
-        image.verify()
-        image = Image.open(io.BytesIO(image_file.read()))
-        image_file.seek(0)
-    except (IOError, SyntaxError) as e:
-        logging.error(f"File is not a valid image: {str(e)}")
-        return jsonify({'error': 'Invalid image file'}), 400
-
-    image_filename = f"{uuid.uuid4()}-{image_file.filename}"
-    firebase_bucket = storage.bucket('cocodiag.appspot.com')
-    blob = firebase_bucket.blob(f"{path}/{user_id}/{image_filename}")
-    blob.upload_from_file(image_file, content_type=image_file.content_type)
-    image_url = blob.public_url  
+        image_filename = f"{uuid.uuid4()}-{image_file.filename}"
+        firebase_bucket = storage.bucket('cocodiag.appspot.com')
+        blob = firebase_bucket.blob(f"{path}/{user_id}/{image_filename}")
+        blob.upload_from_file(image_file, content_type=image_file.content_type)
+       
+        image_url = blob.public_url
+        
+        return image_url
   
-    return image_url
+    except Exception as e:
+        logging.error(f"Failed to upload image to Firebase Storage: {str(e)}")
+        return jsonify({'error': 'Failed to upload image to Firebase Storage'}), 500
